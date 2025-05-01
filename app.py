31037a72e490fbae94c248de3685c1dcdc1931fd
nine9817 <nineninemarketingc@gmail.com>
2025-05-01 02:01:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,4 @@ def upload():
     bg.convert("RGB").save(output_path, "PNG")
     return send_file(output_path, mimetype='image/png')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+
